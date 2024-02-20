b345fca5fd2f728afbcbb6997373a8d97d3fd7fb
erikwibowo <erikwibo@gmail.com>
2024-02-20 13:21:19 +0700

    feat: install jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,16 +2,27 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { AuthDto } from './dto/auth.dto';
 import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(authDto: AuthDto) {
     const user = await this.userService.findByEmail(authDto.email);
     if (user && (await compare(authDto.password, user.password))) {
+      const payload = { sub: user.id, username: user.email };
       const { password, ...result } = user;
-      return result;
+      return {
+        ...result,
+        access_token: await this.jwtService.signAsync(payload, {
+          secret: 'kmzway87aa',
+          expiresIn: '60s',
+        }),
+      };
     }
 
     throw new UnauthorizedException();
