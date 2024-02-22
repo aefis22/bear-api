cd732122fcdb2e6da53151de0a9c9c8da8e4a3ab
erikwibowo <erikwibo@gmail.com>
2024-02-22 14:24:34 +0700

    feat: alter seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -19,21 +19,58 @@ async function main() {
       name: 'Operator',
     },
   });
-  for (let i = 0; i < 100; i++) {
-    const user = await prisma.user.create({
-      data: {
-        name: faker.name.findName(),
-        email: faker.internet.email(),
-        password: await hash('admin', 10),
-      },
-    });
-    await prisma.user_role.create({
-      data: {
-        userId: user.id,
-        roleId: i === 0 ? superadmin.id : admin.id,
-      },
-    });
-  }
+
+  // for (let i = 0; i < 100; i++) {
+  //   await prisma.user.create({
+  //     data: {
+  //       name: faker.name.findName(),
+  //       email: faker.internet.email(),
+  //       password: await hash('admin', 10),
+  //     },
+  //   });
+  // }
+
+  const userSuperadmin = await prisma.user.create({
+    data: {
+      name: 'Superadmin',
+      email: 'superadmin@superadmin.com',
+      password: await hash('superadmin', 10),
+    },
+  });
+  await prisma.user_role.create({
+    data: {
+      userId: userSuperadmin.id,
+      roleId: superadmin.id,
+    },
+  });
+
+  const userAdmin = await prisma.user.create({
+    data: {
+      name: 'Admin',
+      email: 'admin@admin.com',
+      password: await hash('admin', 10),
+    },
+  });
+  await prisma.user_role.create({
+    data: {
+      userId: userAdmin.id,
+      roleId: admin.id,
+    },
+  });
+
+  const userOperator = await prisma.user.create({
+    data: {
+      name: 'Operator',
+      email: 'operator@operator.com',
+      password: await hash('operator', 10),
+    },
+  });
+  await prisma.user_role.create({
+    data: {
+      userId: userOperator.id,
+      roleId: operator.id,
+    },
+  });
 
   await prisma.permission.createMany({
     data: [
@@ -77,6 +114,16 @@ async function main() {
         name: 'permission delete',
       },
     ],
+  });
+
+  const permissions = await prisma.permission.findMany();
+  permissions.map(async (permission) => {
+    await prisma.role_permission.create({
+      data: {
+        roleId: superadmin.id,
+        permissionId: permission.id,
+      },
+    });
   });
   // console.log({ users, roles, permissions });
 }
